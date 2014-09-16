9ea925d5380894f98d4e95e1c51c72e5c1af7e63
dejijaye <dejijaye@gmail.com>
2014-09-16 08:37:56 -0700

    initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+var $overlay = $('<div id="overlay"></div>');
+var $image = $("<img>");
+var $caption = $("<p></p>");
+
+$overlay.append($image);
+
+$overlay.append($caption)
+
+$("body").append($overlay);
+
+
+$("#graphicgallery a").click(function (event) {
+	event.preventDefault();
+	var imageLocation = $(this).attr("href");
+
+	$image.attr("src", imageLocation);
+
+	$overlay.show();
+
+	var captionText = $(this).children(".caption").text();
+	$caption.text(captionText);
+});
+
+$overlay.click(function () {
+	$(this).hide();
+})
